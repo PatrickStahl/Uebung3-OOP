4ad5ed744aadfc9dd233cd9302e56601c6a22e43
Patrick <patrick.stahl@uni-jena.de>
2022-05-22 21:05:49 +0200

    changed stuff

--- Flughafen.java
+++ Flughafen.java
@@ -1,26 +1,42 @@
-//Funktioniert bis jetzt nur mit Flugnummern, den Rest muss ich noch ergänzen
-//die Theorie dahinter passt aber so
-
 public class Flughafen 
 {
     public static void main(String[] args) 
     {
-        Airport airportTest = new Airport(3);
+        Airport airportTest = new Airport(2);
+
+        //ich war bis jetzt zu faul für eine benutztereingabe (btw bin mir gar nicht //
+        //sicher ob man das überhaupt machen soll), weswegen man das hier machen wuss//
+
         Flight flug1 = new Flight();
         flug1.flightNumber=187;
-        
+        flug1.location="Tunesien";
+        flug1.gate="87";
+        flug1.time="15:37";
+        flug1.inOut= true;
+        airportTest.addNewFlight(flug1);
+
         Flight flug2 = new Flight();
-        flug2.flightNumber=420;
+        flug2.flightNumber=318;
+        flug2.location="Malediven";
+        flug2.gate="30";
+        flug2.time="13:37";
+        flug2.inOut= false;
+        airportTest.addNewFlight(flug2);
+        
+        airportTest.removeFlight(318);
 
         Flight flug3 = new Flight();
-        flug3.flightNumber=318;
-        
-        airportTest.addNewFlight(flug1);
-        airportTest.addNewFlight(flug2);
+        flug3.flightNumber=361;
+        flug3.location="America";
+        flug3.gate="56";
+        flug3.time="11:55";
+        flug3.inOut= false;
         airportTest.addNewFlight(flug3);
-        airportTest.removeFlight(flug3.flightNumber);
+
         
+
         airportTest.listDepaturesOnScreen();
+        airportTest.listArrivalsOnScreen();
     
     }    
 }
@@ -37,23 +53,30 @@ class Flight
 class Airport
 {
     private int [] NumberArray;
+    private String [] LocationArray;
+    private String [] GateArray;
+    private String [] TimeArray;
+    private boolean [] InOutArray;
     private int Anzahl;
 
     public Airport (int maxFlights)
     {
         NumberArray = new int[maxFlights];
-        //hier noch location, gate, ... Arrays ergänzen
+        LocationArray = new String[maxFlights];
+        GateArray = new String[maxFlights];
+        TimeArray = new String[maxFlights];
+        InOutArray = new boolean[maxFlights];
         Anzahl = -1;
     }
 
     public void addNewFlight (Flight flight)
     {
-        Anzahl++;
+        Anzahl++; //existiert, damit Programm sieht, ob im Array noch Platz ist
         for(int i = 0; i<NumberArray.length; i++)
         {
             if (NumberArray[i] == flight.flightNumber)
             {
-                System.out.println("Fehler!");
+                System.out.println("Flug existiert bereits");
                 return;
             }
         }
@@ -65,7 +88,12 @@ class Airport
         }
         else
         {
+            System.out.println("Flug " + flight.flightNumber+ " hinzugefügt!");
             NumberArray[Anzahl] = flight.flightNumber;
+            LocationArray[Anzahl] = flight.location;
+            GateArray[Anzahl] = flight.gate;
+            TimeArray[Anzahl] = flight.time;
+            InOutArray[Anzahl] = flight.inOut;
         
         }
     }
@@ -74,21 +102,58 @@ class Airport
     {
         for (int i=0; i<NumberArray.length;i++)
         {
-            if(NumberArray[i]==flightNumber)
+            if(NumberArray[i]==flightNumber) //passende Nummer gefunden
             {
-                NumberArray[i]=0;
+                System.out.println("Flug " + flightNumber+ " entfernt!");
+                for(int j = i; j<NumberArray.length-1;j++) //alle Flüge werden eins vorgezogen, der letzte Flug existiert nun zweimal
+                {
+                    NumberArray[j]=NumberArray[j+1];
+                    LocationArray[j]=LocationArray[j+1];
+                    GateArray[j]=GateArray[j+1];
+                    TimeArray[j]=TimeArray[j+1];
+                    InOutArray[j]=InOutArray[j+1];
+                }
+            }
+        }
+        //letzter Flug aus altem Array wird auf 0 gesetzt, da alle eins aufgerückt sind
+        NumberArray[Anzahl]=0;
+        LocationArray[Anzahl]="-";
+        GateArray[Anzahl]="-";
+        TimeArray[Anzahl]="-";
+        InOutArray[Anzahl]=false;
+        Anzahl--; //Da Flug entfernt wurde ist im Array wieder ein Platz mehr frei
+    }
+    
+    public void listDepaturesOnScreen() //ausgehend
+    {
+        System.out.println("");
+        System.out.println("Ausgehende Fluege:");
+        for(int i = 0; i<NumberArray.length;i++)
+        {
+            if((NumberArray[i]!=0) && (InOutArray[i]==false))
+            {
+                System.out.println("Flugnummer: "  + NumberArray[i]);
+                System.out.println("Zielort: " + LocationArray[i]);
+                System.out.println("Gate: " + GateArray[i]);
+                System.out.println("Ankunftszeit: " + TimeArray[i]);
+                System.out.println("");
             }
         }
     }
-    //funktioniert logischerweise noch nicht für in und out weil ich das noch nicht gemacht hab
-    public void listDepaturesOnScreen() 
+    
+    public void listArrivalsOnScreen() //eingehend
     {
+        System.out.println("");
+        System.out.println("Eingehende Fluege:");
         for(int i = 0; i<NumberArray.length;i++)
         {
-            if(NumberArray[i]!=0)
+            if((NumberArray[i]!=0) && (InOutArray[i]==true))
             {
-                System.out.println("Flug " + (i+1) + ":");
                 System.out.println("Flugnummer: "  + NumberArray[i]);
+                System.out.println("Abflugsort: " + LocationArray[i]);
+                System.out.println("Gate: " + GateArray[i]);
+                System.out.println("Abflugszeit: " + TimeArray[i]);
+                System.out.println("");
             }
         }
     }
