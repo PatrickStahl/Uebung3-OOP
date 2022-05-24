af03d19bc06f7711562a2cb054d56289c6b9fd30
Patrick <patrick.stahl@uni-jena.de>
2022-05-24 14:52:45 +0200

    changes

--- Adresse.java
+++ Adresse.java
@@ -1,81 +1,81 @@
 
-/* Das habe ich noch nicht gemacht*/ 
+// /* Das habe ich noch nicht gemacht*/ 
 
 
-class Adresse
-{
+// class Adresse
+// {
 
-    private String straße;
-    private String plz;
-    private String ort;
+//     private String straße;
+//     private String plz;
+//     private String ort;
 
-    public void Adresse (String straße, String plz, String ort)
-    {
-        this.straße = straße;
-        this.plz = plz;
-        this.ort = ort;
-    }
-    public String getStraße()
-    {
-        return straße;
-    }
-    public String getPlz()
-    {
-        return plz;
-    }
-    public String getOrt()
-    {
-        return ort;
-    }
+//     public void Adresse (String straße, String plz, String ort)
+//     {
+//         this.straße = straße;
+//         this.plz = plz;
+//         this.ort = ort;
+//     }
+//     public String getStraße()
+//     {
+//         return straße;
+//     }
+//     public String getPlz()
+//     {
+//         return plz;
+//     }
+//     public String getOrt()
+//     {
+//         return ort;
+//     }
 
-    public static void main(String[] args)
-    {
+//     public static void main(String[] args)
+//     {
         
-    }
-}
+//     }
+// }
 
-class Hörsaal
-{
-    private int plätze;
-    private int Vorlesung [];
+// class Hörsaal
+// {
+//     private int plätze;
+//     private int Vorlesung [];
 
-    public void Hörsaal(int plz, Adresse adr, Vorlesung[] vlg)
-    {
-        this.plätze = plz;
-        Adresse Hörsaal=new Adresse();
-        adr = Hörsaal.Adresse("", "", "");
-        //plz=Hörsaal.getStraße();
-        //adr=Hörsaal.Adresse();
+//     public void Hörsaal(int plz, Adresse adr, Vorlesung[] vlg)
+//     {
+//         this.plätze = plz;
+//         Adresse Hörsaal=new Adresse();
+//         adr = Hörsaal.Adresse("", "", "");
+//         //plz=Hörsaal.getStraße();
+//         //adr=Hörsaal.Adresse();
 
-    }
+//     }
 
-    int getPlätze()
-    {
-        return plätze;
-    }
-    void getAdresse()
-    {
-        Adresse AdresseHörsaal = new Adresse();
-        // String Postleitzahl = test.plz;
-        // return Postleitzahl;
-        AdresseHörsaal.getPlz();
-        AdresseHörsaal.getStraße();
-        AdresseHörsaal.getOrt();
+//     int getPlätze()
+//     {
+//         return plätze;
+//     }
+//     void getAdresse()
+//     {
+//         Adresse AdresseHörsaal = new Adresse();
+//         // String Postleitzahl = test.plz;
+//         // return Postleitzahl;
+//         AdresseHörsaal.getPlz();
+//         AdresseHörsaal.getStraße();
+//         AdresseHörsaal.getOrt();
 
-    }
-}
+//     }
+// }
 
-class SeminarRaum
-{
+// class SeminarRaum
+// {
 
-}
+// }
 
-class Labor
-{
-    int plätze;
-}
+// class Labor
+// {
+//     int plätze;
+// }
 
-class Vorlesung
-{
+// class Vorlesung
+// {
 
-}
+// }
